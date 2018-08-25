d9f1bbc57d9009d962b6225ab817dbd2f5ba9273
Chong Sun <sonshicyu@gmail.com>
2018-08-24 23:59:03 -0500

    Add Regression class that inherit from Perceptron

--- regression.py
+++ regression.py
@@ -1,75 +1,8 @@
 import numpy as np
 import sys
+import perceptron as pp
 
-def sign(x):
-    if np.sign(x)==0:
-        return -1
-    else:
-        return int(np.sign(x))
-
-def findLine(point1, point2):
-    slope = (point2[1]-point1[1])/float(point2[0]-point1[0])
-    intercept = point2[1] - slope*point2[0]
-    return slope, intercept
-
-def classify(data, slope, intercept):
-    group = []
-    size = np.size(data, 0)
-    for i in range(size):
-        group.append(sign(data[i,1]-slope*data[i,0]-intercept))
-    return group
-
-def findErrorRate(group1, group2):
-    error = 0
-    length = len(group1)
-    for i in range(length):
-        if group1[i] != group2[i]:
-            error += 1
-    return float(error)/length
-
-class Perceptron: 
-    def __init__(self, data, group):
-        self.size = np.size(data, 0) 
-        self.data = np.concatenate((np.ones((self.size,1)), data), axis=1) 
-        self.correctGroup = group 
-        self.misclassified = list(range(self.size)) 
-        self.coeff = np.zeros((np.size(data, 1)+1, 1))
-        self.classifiedGroup = [0] * self.size
-        self.repeat = 0
-
-    def updateMisclassified(self):
-        self.misclassified = []
-        for i in range(self.size):
-           if(self.classifiedGroup[i] != self.correctGroup[i]):
-              self.misclassified.append(i) 
-        return self.misclassified
-
-    def recalculateClassified(self):
-        for i in range(self.size):
-            self.classifiedGroup[i] = sign(self.coeff.T.dot(self.data[i, :].reshape(-1, 1)))
-        return self.classifiedGroup
-
-    def updateCoeff(self):
-        for index in self.misclassified:
-            self.coeff = self.coeff + (self.correctGroup[index]*self.data[index, :]).reshape(-1, 1)
-        return self.coeff
-    
-    def learn(self):
-        while self.misclassified != []:
-            self.repeat += 1
-            self.updateCoeff()
-            self.recalculateClassified()
-            self.updateMisclassified()
-        return self.repeat
-
-    def getCoeff(self):
-        return self.coeff
-
-    def getRepetition(self):
-        return self.repeat
-
-
-def Regression(Perceptron):
+def Regression(pp.Perceptron):
     def getHatMatrix():
         pseudoInv = np.linalg.inv(self.data.T.dot(self.data)) * self.data
         self.hatMatrix = self.data * pseudoInv
@@ -77,7 +10,7 @@ def Regression(Perceptron):
 
     def classify():
         for i in range(self.size):
-            self.classifiedGroup[i] = sign(self.data[i, :].dot(self.hatMatrix))
+            self.classifiedGroup[i] = pp.sign(self.data[i, :].dot(self.hatMatrix))
         return self.classifiedGroup
     
     def getMisclassified(): 
@@ -93,6 +26,7 @@ def Regression(Perceptron):
         self.classify()
         self.getMisclassified()
         return self.getInSampleErr()
+
 
 
 if __name__ == "__main__":
